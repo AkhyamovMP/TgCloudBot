77d02de3c324024301538a676dc73169bdffff8e
myusername <AkhyamovMP@gmail.com>
2022-06-04 12:44:51 +0300

    forward to channel

--- main.py
+++ main.py
@@ -11,8 +11,18 @@ print(manager.translate_folder_id(1))
 class User:
     def __init__(self):
         self.state = 'SETUP'
-        self.channel_id = ''
+        self.channel_id = '-1001559868838'
         self.folders = {}
+
+    def forward_message(self, msg):
+        res = requests.get('https://api.telegram.org/bot%s/forwardMessage?chat_id=%s&from_chat_id=%s&message_id=%s' % (config.bot_token, self.channel_id, msg.chat.id, msg.id))
+
+    #def create_folder(self, ):
+
+class Folder:
+    def __init__(self):
+        self.name = ''
+        self.id = ''
 
 
 def get_user(msg):
@@ -34,15 +44,23 @@ def start(msg, res=False):
     bot.send_message(msg.chat.id, main_message)
 
 
+@bot.message_handler(func=lambda msg: get_user(msg).state == 'SETUP')
+def setup_message(msg):
+    post_message = 'MshCloudBot initial message'
+    if msg.forward_from_chat.id and requests.get('https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s' % (config.bot_token, msg.forward_from_chat.id, post_message)).ok:
+        bot.send_message(msg.chat.id, msg)
+        get_user(msg).channel_id =  msg.forward_from_chat.id
+        get_user(msg).state = 'MAIN_MENU'
+    else:
+        bot.send_message(msg.chat.id, 'У меня нет доступа к этому каналу')
+
+
 
 @bot.message_handler(commands=["help"])
 def print_commands(msg):
     bot.send_message(msg.chat.id, 'TODO: список команд')
 
 
-def post(msg):
-    post_message = 'new message!'
-    res = requests.get('https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s' % (config.bot_token, channel_id, post_message))
 
 
 @bot.message_handler(func=lambda msg: get_user(msg).state == 'MAIN_MENU')
@@ -50,14 +68,7 @@ def main(message):
     bot.send_message(message.chat.id, 'there is main menu')
 
 
-@bot.message_handler(content_types=["text"])
-def setup_message(msg):
-    post_message = 'MshCloudBot initial message'
-    if msg.forward_from_chat.id and requests.get('https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s' % (config.bot_token, msg.forward_from_chat.id, post_message)).ok:
-        bot.send_message(msg.chat.id, msg)
-        get_user(msg).channel_id =  msg.forward_from_chat.id
-    else:
-        bot.send_message(msg.chat.id, 'У меня нет доступа к этому каналу')
+
 
 
 bot.infinity_polling()
